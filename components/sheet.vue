25e73cce235beb752df78f4cddcb761215da5655
Brandon Gomes <brandongomes1705@gmail.com>
2023-06-23 14:15:02 +0100

    Adjusted animation timings

--- components/sheet.vue
+++ components/sheet.vue
@@ -233,8 +233,8 @@ export const SheetContent = defineComponent({
         <Transition
           enter-from-class="opacity-0"
           leave-to-class="opacity-0"
-          enter-active-class="transition-opacity duration-200 ease-out"
-          leave-active-class="transition-opacity duration-150 ease-in"
+          enter-active-class="transition-opacity duration-500 ease-out"
+          leave-active-class="transition-opacity duration-200 ease-in"
         >
           {api.value.isOpen && (
             <div
@@ -247,8 +247,8 @@ export const SheetContent = defineComponent({
         <Transition
           enter-from-class={`${translateClass()}`}
           leave-to-class={`${translateClass()}`}
-          enter-active-class="transition duration-200 ease-out"
-          leave-active-class="transition duration-150 ease-in"
+          enter-active-class="transition duration-500 ease-out"
+          leave-active-class="transition duration-200 ease-in"
         >
           {api.value.isOpen && (
             <div
