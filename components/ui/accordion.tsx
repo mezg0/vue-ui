8dac5d17bcf4c1fbe1ddd36d84b01becae567dd4
Brandon Gomes <brandongomes1705@gmail.com>
2023-06-27 11:09:39 +0100

    Added accordion

---
+++ components/ui/accordion.tsx
@@ -0,0 +1,91 @@
+import {
+  Accordion,
+  AccordionTrigger as AccordionTriggerPrimitive,
+  AccordionItem as AccordionItemPrimitive,
+  AccordionContent as AccordionContentPrimitive,
+} from "@ark-ui/vue";
+import { cn } from "~/lib/utils";
+
+const AccordionItem = defineComponent({
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+  setup({ value }, { slots, attrs }) {
+    return () => (
+      <AccordionItemPrimitive
+        class={cn("border-b", attrs.class ?? "")}
+        value={value}
+        {...attrs}
+      >
+        {slots.default?.()}
+      </AccordionItemPrimitive>
+    );
+  },
+});
+
+const AccordionTrigger = defineComponent({
+  setup(_, { slots, attrs }) {
+    return () => (
+      <AccordionTriggerPrimitive>
+        <button
+          class={cn(
+            "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
+            attrs.class ?? ""
+          )}
+          {...attrs}
+        >
+          {slots.default?.()}
+        </button>
+      </AccordionTriggerPrimitive>
+    );
+  },
+});
+
+const AccordionContent = defineComponent({
+  setup(_, { slots, attrs }) {
+    const content = ref<HTMLElement | null>(null);
+    const container = ref<HTMLElement | null>(null);
+
+    const updateContentHeight = () => {
+      if (!content.value || !container.value) return;
+
+      container.value.style.setProperty("height", "auto");
+      const contentHeight = getComputedStyle(content.value).height;
+
+      container.value.style.setProperty("--content-height", contentHeight);
+
+      content.value.style.setProperty("height", null);
+    };
+
+    onMounted(() => {
+      updateContentHeight();
+      window.addEventListener("resize", updateContentHeight);
+    });
+
+    onBeforeMount(() => {
+      window.removeEventListener("resize", updateContentHeight);
+    });
+
+    return () => (
+      <div ref={container}>
+        <AccordionContentPrimitive
+          class={cn(
+            "overflow-hidden block text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down h-0 data-[expanded]:h-[var(--content-height)] duration-300 ease-in-out",
+            attrs.class ?? ""
+          )}
+          {...attrs}
+        >
+          <div class="pb-4 pt-0" ref={content}>
+            {slots.default?.()}
+          </div>
+        </AccordionContentPrimitive>
+      </div>
+    );
+  },
+});
+
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent };
+
---
+++ content/component/accordion.md
@@ -0,0 +1,42 @@
+# Accordion
+
+A modal dialog that interrupts the user with important content and expects a response.
+
+
+## Preview
+::component-example{src="/components/content/accordion/accordion-preview.vue"}
+#preview
+  :::accordion-preview
+  :::
+::
+
+## Installation
+
+### Copy and paste this into your project
+::component-source{src="/components/ui/accordion.tsx"}
+::
+
+## Usage
+::code-block
+```ts
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from "~/components/ui/accordion";
+```
+::
+
+::code-block
+```html
+<Accordion type="single" collapsible>
+  <AccordionItem value="item-1">
+    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+    <AccordionContent>
+      Yes. It adheres to the WAI-ARIA design pattern.
+    </AccordionContent>
+  </AccordionItem>
+</Accordion>
+```
+::
