64c78413f7730fcd9b2aa9e26e61db64b3c7df83
Brandon Gomes <brandongomes1705@gmail.com>
2023-06-27 13:31:25 +0100

    Fix accordion target

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -33,7 +33,7 @@ const AccordionTrigger = defineComponent({
       <AccordionTriggerPrimitive>
         <button
           class={cn(
-            "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
+            "w-full flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
             attrs.class ?? ""
           )}
           {...attrs}
